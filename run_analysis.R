8d3186924b7415ee6170404c44249eb0f9320f1b
Asif2201 <asifrafatkhan@me.com>
2017-05-24 04:42:29 +0200

    first commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,7 @@
 # Script to combine and clean data based on the data set provided by: 
-# Use of this dataset in publications must be acknowledged by referencing the following publication [1] 
+# 
 # [1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones 
-# using a Multiclass Hardware-Friendly Support Vector Machine. 
-# International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
-# This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions 
-# for its use or misuse. Any commercial use is prohibited.
-# Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.
+#
 # The script does the following actions to produce the clean data set
 # 1. Merges the training and the test sets to create one data set.
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
@@ -13,6 +9,25 @@
 # 4. Appropriately labels the data set with descriptive variable names.
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 # the data must be in directory UCI HAR Dataset located in the working directory
+
+# Function to convert a list of strings into a data frame splitting when a CRLF character is found in the string
+# input parameters x : List of strings
+splitpernewline <- function(x)     {
+        startingindex = 1
+        # find indices where CR LF character is found
+        IndiceswithNewLine = grep("\r\n", x)
+        # create an empty matrix of specificied rows and columns
+        ReturnMatrix <- matrix(0, ncol = 561, nrow = length(IndiceswithNewLine))
+        # Add rows to the Matrix with string ending where CR LF is found 
+        for(i in 1 : length(IndiceswithNewLine)) {
+                y <- x[startingindex:IndiceswithNewLine[i]]    
+                startingindex <- IndiceswithNewLine[i] + 1
+                # add into matrix as numeric which also removes the CR LF characters
+                ReturnMatrix[i, ] <- as.numeric(y)
+        }
+        #return the matrix as data frame without factoring
+        data.frame(ReturnMatrix, stringsAsFactors = FALSE)
+}
 
 library(sqldf)
 # Load reference data for activities and features
@@ -80,21 +95,3 @@ colnames(cleanDF)[3] <- "activitydescription"
 write.table(x = cleanDF, file = "cleandataset.txt", row.names = FALSE)
 
 
-# Function to convert a list of strings into a data frame splitting when a CRLF character is found in the string
-# input parameters x : List of strings
-splitpernewline <- function(x)     {
-        startingindex = 1
-        # find indices where CR LF character is found
-        IndiceswithNewLine = grep("\r\n", x)
-        # create an empty matrix of specificied rows and columns
-        ReturnMatrix <- matrix(0, ncol = 561, nrow = length(IndiceswithNewLine))
-        # Add rows to the Matrix with string ending where CR LF is found 
-        for(i in 1 : length(IndiceswithNewLine)) {
-                y <- x[startingindex:IndiceswithNewLine[i]]    
-                startingindex <- IndiceswithNewLine[i] + 1
-                # add into matrix as numeric which also removes the CR LF characters
-                ReturnMatrix[i, ] <- as.numeric(y)
-        }
-        #return the matrix as data frame without factoring
-        data.frame(ReturnMatrix, stringsAsFactors = FALSE)
-}
